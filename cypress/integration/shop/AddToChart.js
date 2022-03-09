5c1aaf4a5c169b628a19ee0512e8a2b929eeabf2
agomulka <mrrixo@gmail.com>
2022-03-10 00:48:57 +0100

    committestarena

--- cypress/integration/shop/AddToChart.js
+++ cypress/integration/shop/AddToChart.js
@@ -2,10 +2,15 @@ import {AddToChart} from "../../pages/shopPage/ShopPage"
 
 const addToChart = new AddToChart()
 
-it('addtochart', function() {
+it('addtochart Proccess', function() {
 
-addToChart.navigate('http://automationpractice.com/index.php')
-addToChart.clickAddChart()
-addToChart.order_SingIn('testowekonto18@gmail.com', 'Artur', 'Kowalski', 'Qwerty123', '12', 'Krakow', '32310', '123123123')
-addToChart.order_Proccess()
+addToChart.navigate('http://demo.testarena.pl/zaloguj')
+addToChart.clickAddChart('arturgomulka20@gmail.com', 'Qwerty123', 'pierwsze wydanie')
+// addToChart.order_SingIn('testowekonto102@gmail.com', 'Artur', 'Kowalski', 'Qwerty1234', '12', 'Krakow', '32-310', '123123123')
+// addToChart.order_Proccess()
 })
+
+// it('singIn Fail', function() {
+// addToChart.order_SingIn('testowekonto102@gmail.com', 'Artur', 'Kowalski', 'Qwerty12345', '12', 'Krakow', '32310', '123123123')
+// addToChart.order_Proccess()
+// })
--- cypress/pages/shopPage/ShopPage.js
+++ cypress/pages/shopPage/ShopPage.js
@@ -1,4 +1,4 @@
-import {ShopPage, ProceedToCheckout, AddToChart_BlouseButton, ProceedGreen, EmailAddress, CreateAccountButton, MrCheck, FirstName, LastName, Password, Days, Month, Years, NewsletterCheck, SpecialOffersCheck, Address, City, State, PostCode, MobilePhone, Register, AgreeTermsCheck, PayByBankWireButton, ConfirmOrderButton, OrderSuccessText} from "../shopPage/ShopPageSelectors"
+import {ShopPage, ProceedToCheckout, AddToChart_BlouseButton, ProceedGreen, EmailAddress, CreateAccountButton, MrCheck, FirstName, LastName, Password, Days, Month, Years, NewsletterCheck, SpecialOffersCheck, Address, City, State, PostCode, MobilePhone, Register, AgreeTermsCheck, PayByBankWireButton, ConfirmOrderButton, OrderSuccessText, SingInButton, SingOutButton, PostCodeErrorText} from "../shopPage/ShopPageSelectors"
 
 export class AddToChart{
     
@@ -6,43 +6,63 @@ export class AddToChart{
         cy.visit(url)
     }
     
-    clickAddChart(){
-        cy.get(AddToChart_BlouseButton).click()
-        cy.get(ProceedToCheckout).click()
+    clickAddChart(email, Password, nazwa){
+        cy.get(AddToChart_BlouseButton).type(email)
+        cy.get(ProceedToCheckout).type(Password)
         cy.get(ProceedGreen).click()
-    }
-
-    order_SingIn(email, name, secondname, pass, addressName, CityName, Code, PhoneNumber){
-        cy.get(EmailAddress).click()
-        cy.get(EmailAddress).type(email)
-     
+        cy.get(SingInButton).click()
+        cy.get(SingOutButton).click()
+        cy.get(EmailAddress).type(nazwa)
         cy.get(CreateAccountButton).click()
         cy.get(MrCheck).click()
-        cy.get(FirstName).type(name)
-        cy.get(LastName).type(secondname)
-        cy.get(Password).type(pass)
-        cy.get(Days).select('1').should('have.value', '1')
-        cy.get(Month).select('1').should('have.value', '1')
-        cy.get(Years).select('2020')
-        cy.get(NewsletterCheck).click()
-        cy.get(SpecialOffersCheck).click()
-        cy.get(Address).type(addressName)
-        cy.get(City).type(CityName)
-        cy.get(State).select('1')
-        cy.get(PostCode).type(Code)
-        cy.get(MobilePhone).type(PhoneNumber)
-        cy.get(Register).click()
     }
 
-    order_Proccess(){
-        cy.get(ProceedGreen).click()
-        cy.get(AgreeTermsCheck).click()
-        cy.get(ProceedGreen).click()
-        cy.get(PayByBankWireButton).click()
-        cy.get(ConfirmOrderButton).click()
-        cy.get(OrderSuccessText).contains('Your order on My Store is complete.')
+    // order_SingIn(email, name, secondname, pass, addressName, CityName, Code, PhoneNumber){
+    //     cy.get(SingInButton).click()
+    //     cy.get(EmailAddress).click()
+    //     cy.get(EmailAddress).type(email)
+    //     cy.get(CreateAccountButton).click()
+    //     cy.get(MrCheck).click()
+    //     cy.get(FirstName).type(name)
+    //     cy.get(LastName).type(secondname)
+    //     cy.get(Password).type(pass)
+    //     cy.get(Days).select('1')
+    //     cy.get(Month).select('1')
+    //     cy.get(Years).select('2020')
+    //     cy.get(NewsletterCheck).click()
+    //     cy.get(SpecialOffersCheck).click()
+    //     cy.get(Address).type(addressName)
+    //     cy.get(City).type(CityName)
+    //     cy.get(State).select('1')
+    //     cy.get(PostCode).type(Code)
+    //     cy.get(MobilePhone).type(PhoneNumber)
+    //     cy.get(Register).click()
+    //     if( cy.get(PostCodeErrorText)=='The Zip/Postal code you\'ve entered is invalid. It must follow this format: 00000')
+    //     {
+            
+    //         cy.get(Password).type(pass)
+    //         cy.get(Register).click()  
+           
+    //     }
+    //     else{
+          
         
-    }
+    //     cy.get(PostCodeErrorText).should('have.text', 'The Zip/Postal code you\'ve entered is invalid. It must follow this format: 00000')
+    //     }
+    // }
+
+    // order_Proccess(){
+    //     cy.get(ProceedGreen).click()
+    
+    //     cy.get(ProceedGreen).click()
+    //     cy.get(AgreeTermsCheck).click()
+    //     cy.get(ProceedGreen).click()
+    //     cy.get(PayByBankWireButton).click()
+    //     cy.get(ConfirmOrderButton).click()
+    //     cy.get(OrderSuccessText).contains('Your order on My Store is complete.')
+    //     cy.get(SingOutButton).click()
+        
+    // }
 
     
     
